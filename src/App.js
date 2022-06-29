94f335835bbe4ceee390b4ed8cc59a3c36c69182
Augie Bren <Augie>
2022-06-28 21:42:21 -0500

    end of class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
 import './App.css'
 
 
 function App() {
+  const [gitHubName, setGitHubName] = useState('')
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/Augiedog')
+    .then(res => res.json())
+    .then(data => {
+      setGitHubName(data.name)
+      // url
+      // image
+    })
+  }, [])
+
   return (
     <div className="App">
       <h1>Github Profile Info:</h1>
+      <h2>{gitHubName}</h2>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,20 @@
+import App from './App'
+import { render, screen, waitFor } from '@testing-library/react'
 
+beforeEach(() => {
+    fetch.resetMocks()
+})
 
+test("recevies GitHub name from Github", async () => {
+    fetch.mockResponseOnce(JSON.stringify({name: 'AugieDog'}))
+    render(<App />)
+    const gitHubName = await waitFor(() => screen.getByRole('heading', {level: 2 }))
+    expect(gitHubName).toHaveTextContent('AugieDog')
+})
 
+test("link to github profile", async () => {
+    fetch.mockResponseOnce(JSON.stringify({html_url: "https://github.com/Augiedog"}))
+    render(<App />)
+    const gitHubUrl = await waitFor(() => screen.getByRole('herf'))
+    expect(gitHubUrl).toHaveTextContent('https://github.com/Augiedog')
+})
